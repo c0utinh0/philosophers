637b2b9dbfc8dbff890f80bc46e32e09c14e776c
dcoutinh <douglas-couto@hotmail.com>
2022-11-24 18:34:04 -0300

    First commit

---
+++ philo/philo.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcoutinh <dcoutinh@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 16:06:59 by dcoutinh          #+#    #+#             */
+/*   Updated: 2022/11/24 15:04:53 by dcoutinh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_philo
+{
+	int				philo;
+	struct s_philo	*right;
+	struct s_philo	*left;
+}	t_philo;
+
+#endif
