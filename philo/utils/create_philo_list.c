082d165204e48cec18235197825840206d864500
dcoutinh <douglas-couto@hotmail.com>
2022-12-08 15:38:51 -0300

    corrigido a criacao das threads

--- philo/utils/create_philo_list.c
+++ philo/utils/create_philo_list.c
@@ -6,7 +6,7 @@
 /*   By: dcoutinh <dcoutinh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 15:47:35 by dcoutinh          #+#    #+#             */
-/*   Updated: 2022/12/06 17:29:20 by dcoutinh         ###   ########.fr       */
+/*   Updated: 2022/12/08 15:30:40 by dcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,19 @@ static void add_philo_list(t_philo **list, int id, char **argv, int argc)
         printf("Memory Allocation Error!\n");
 }
 
+static void create_first(t_philo **philos)
+{
+    t_philo *philo;
+
+    philo = *philos;
+    while (philo != NULL) // tratar
+    {
+        if (philo->right == NULL)
+            philo->first = *philos;
+        philo = philo->right;
+    }
+}
+
 void create_philos(t_philo **philos, char **argv, int argc)
 {
     int count_philo;
@@ -49,11 +62,6 @@ void create_philos(t_philo **philos, char **argv, int argc)
     count_philo = ft_atoi(argv[1]);
     while (count_philo)
         add_philo_list(&philo, count_philo--, argv, argc);
-    // while (philo != NULL) // tratar
-    // {
-    //     if (philo->right == NULL)
-    //         philo->first = philo;
-    //     philo = philo->right;
-    // }
     *philos = philo;
+	create_first(philos);
 }
