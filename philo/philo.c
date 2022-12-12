7a26e2502356f3446b3e47bdd1c0bdb3cf876bf6
dcoutinh <douglas-couto@hotmail.com>
2022-12-12 14:35:17 -0300

    corrigido

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: dcoutinh <dcoutinh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 14:54:34 by dcoutinh          #+#    #+#             */
-/*   Updated: 2022/12/12 14:12:50 by douglas          ###   ########.fr       */
+/*   Updated: 2022/12/12 14:16:20 by dcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@ int main(int argc, char *argv[])
 {
 	t_simulation *simulation;
 
+	printf("chech 01\n");
 	simulation = malloc(sizeof(t_simulation));
+	printf("chech 02\n");
 	create_philos(argv, argc, &simulation);
+	printf("chech 03\n");
 	threads(&simulation);
+	printf("chech end\n");
 
 	return (0);
 }
