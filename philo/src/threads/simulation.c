a9168022c0683d57e980e40bd004d768168d2cd0
dcoutinh <douglas-couto@hotmail.com>
2022-12-20 09:57:09 -0300

    tratado simulacao para 1 philo

--- philo/src/threads/simulation.c
+++ philo/src/threads/simulation.c
@@ -6,30 +6,30 @@
 /*   By: dcoutinh <dcoutinh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 16:43:26 by dcoutinh          #+#    #+#             */
-/*   Updated: 2022/12/19 14:02:09 by dcoutinh         ###   ########.fr       */
+/*   Updated: 2022/12/20 09:56:12 by dcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../philo.h"
 
+
+void one_philo_simulation(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->m_fork);
+	if(*philo->is_died == 0)
+		print_action("has taken a fork", current_timestamp(), philo);
+	philo->last_eat = current_timestamp();
+	usleep(philo->time_to_eat * 1000);
+	pthread_mutex_unlock(&philo->m_fork);
+}
+
 void *thread_simulation(void *temp)
 {
 	t_philo *philo = (t_philo *)temp;
 
-	if (philo->times_must_eat > 0 )
-	{
-		while (*(philo)->is_died == 0)
-		{	
-			if(*philo->is_died == 0)
-				is_eating(philo);
-			if(*philo->is_died == 0)
-				is_sleeping(philo);
-			if(*philo->is_died == 0)
-				is_thinking(philo);
-		}
-	}
+	if(philo->number_of_philosophers == 1)
+		one_philo_simulation(philo);
 	else
-	{
 		while (*(philo)->is_died == 0)
 		{
 			if(*philo->is_died == 0)
@@ -38,7 +38,6 @@ void *thread_simulation(void *temp)
 				is_sleeping(philo);
 			if(*philo->is_died == 0 )
 				is_thinking(philo);
-		}
 	}
 	return NULL;
 }
